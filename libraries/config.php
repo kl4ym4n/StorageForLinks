edfe3757c5da942338a4c471d2ddcfa6acea8df6
Klayman <kl4ym4n@gmail.com>
2015-12-14 16:50:02 +0600

    config

---
+++ libraries/config.php
@@ -0,0 +1,7 @@
+<?php
+$mailer_config['from_name'] = 'Me - Creator';
+$mailer_config['from_email'] = 'email@mywebsite.com';
+$mailer_config['smtp_mode'] = 'enabled'; // enabled or disabled
+$mailer_config['smtp_host'] = 'smtp.gmail.com';
+$mailer_config['smtp_port'] = 587;
+$mailer_config['smtp_username'] = 'kl4ym4n@gmail.com';
