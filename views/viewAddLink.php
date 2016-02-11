b4e12830014775c0e03d0792d6f133e40da657a8
Klayman <kl4ym4n@gmail.com>
2016-02-11 18:22:12 +0600

    add link view

---
+++ views/viewAddLink.php
@@ -0,0 +1,10 @@
+<h1>Add new link</h1>
+
+<form method = post action = "">
+
+    Header: <input type = text name = "linkheader" value = ""></br>
+    Link: <input type = text name = "userlink" value = ""></br>
+    Description: <textarea rows="10" cols="75" name="linkdescription" ></textarea></br>
+    Private: <input type = checkbox name = "linkflag" value = "0"></br>
+    <input type = submit name = "loginbutton" value = "Enter!">
+</form>
