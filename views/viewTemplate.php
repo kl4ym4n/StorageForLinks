ce23c84bb6726f35418ec3aef6dcd6f03d28ff9c
Klayman <kl4ym4n@gmail.com>
2016-02-19 18:02:09 +0600

    include bootstrap

--- views/viewTemplate.php
+++ views/viewTemplate.php
@@ -5,6 +5,8 @@
     <title>Link storage</title>
 </head>
 <body>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <?php include 'views/'.$content_view; ?>
 </body>
 </html>
