060d703f9d74d8ddefb0376096768dc244f480b8
Klayman <kl4ym4n@gmail.com>
2016-02-19 18:00:26 +0600

    add empty methods

--- core/generalView.php
+++ core/generalView.php
@@ -1,6 +1,26 @@
 <?php
 class View
 {
+    public $template;
+    public $args = array();
+
+    public function _toString()
+    {
+        ob_start();
+
+        ob_end_clean();
+    }
+
+    public function render()
+    {
+
+    }
+
+    public function setParameters()
+    {
+
+    }
+
     function generate($content_view, $template_view, $data = null)
     {
         include 'views/'.$template_view;
