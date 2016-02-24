891d3ba409926b3ed9968b89a01c713d95e8a953
Klayman <kl4ym4n@gmail.com>
2016-02-24 17:12:30 +0600

    add fixes

--- views/viewTemplate.php
+++ views/viewTemplate.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="utf-8">
     <title>Link storage</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
-<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
 <?php include 'views/'.$content_view; ?>
 </body>
 </html>
