ec99b8236d8808ae27e8f3caa44ef51ce41164bf
Klayman <kl4ym4n@gmail.com>
2016-02-24 17:08:25 +0600

    add head includes

---
+++ views/viewHead.php
@@ -0,0 +1,13 @@
+<?php
+class ViewHead extends View
+{
+    public function __construct()
+    {
+        $this->template = '<meta charset="utf-8">
+        <title>Link storage</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>';
+    }
+}
+
+
