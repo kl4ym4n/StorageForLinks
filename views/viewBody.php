3cfe0b80dc77bed0d67caba51d5dc3c5eca97b7d
Klayman <kl4ym4n@gmail.com>
2016-02-24 17:06:43 +0600

    add body view

---
+++ views/viewBody.php
@@ -0,0 +1,21 @@
+<?php
+include_once "viewHeader.php";
+include_once "viewContent.php";
+include_once "viewFooter.php";
+class ViewBody extends View
+{
+    public $header, $content, $footer;
+    public function __construct()
+    {
+        $this->template = '<!DOCTYPE html>
+                          <div class = "header"> %s </div>
+                          <div class = "content"> %s </div>
+                          <div class = "footer"> %s </div>';
+
+        $this->header = new ViewHeader();
+        $this->content = new ViewContent();
+        $this->footer = new ViewFooter();
+
+        $this->args = array($this->header, $this->content, $this->footer);
+    }
+}
