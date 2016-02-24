0c047b0ea774fd7384abcbd0572aab735d67c3a2
Klayman <kl4ym4n@gmail.com>
2016-02-24 17:10:01 +0600

    add new view creation

--- controllers/controllerIndex.php
+++ controllers/controllerIndex.php
@@ -1,8 +1,11 @@
 <?php
+include_once "views/viewIndex.php";
 class ControllerIndex extends Controller
 {
     public function actionIndex()
     {
-        $this->view->generate('viewIndex.php', 'viewTemplate.php');
+        //$this->view->generate('viewIndex.php', 'viewTemplate.php');
+        $this->view = new MainView();
+        $this->view->render();
     }
 }
