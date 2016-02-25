ffee2eed29d969ff6d15a8edc82afe8f3ed9f233
Klayman <kl4ym4n@gmail.com>
2016-02-25 18:26:49 +0600

    add autoloader include

--- bootstrap.php
+++ bootstrap.php
@@ -3,4 +3,5 @@ require_once 'core/generalModel.php';
 require_once 'core/generalView.php';
 require_once 'core/generalController.php';
 require_once 'core/route.php';
+require_once 'autoloader.php';
 Route::start();
