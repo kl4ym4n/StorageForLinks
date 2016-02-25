16b8815f85bf6706822ac124347221264b3f653a
Klayman <kl4ym4n@gmail.com>
2016-02-25 18:26:10 +0600

    add autoloader logic

---
+++ autoloader.php
@@ -0,0 +1,29 @@
+<?php
+
+function autoload($className)
+{
+    $arrayPaths = array(
+        '/views/',
+        '/models/',
+        '/controllers/'
+    );
+
+    # Count the total item in the array.
+    $totalPaths = count($arrayPaths);
+
+    # Set the class file name.
+    $fileName = lcfirst($className).'.php';
+
+    # Loop the array.
+    foreach($arrayPaths as $path)
+    {
+        $file = sprintf('%s%s%s', __DIR__, $path, $fileName);
+        if(is_file($file))
+        {
+            include_once $file;
+        }
+
+    }
+}
+
+spl_autoload_register('autoload');
