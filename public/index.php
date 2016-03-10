29b8f03f9ceff0da761cd63843c88c57b6c1e3cf
Klayman <kl4ym4n@gmail.com>
2016-03-10 18:22:32 +0600

    move index here

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+$user = 'root';
+$pass = 'azarta';
+try
+{
+    $connection = new PDO('mysql:host=localhost; dbname=mydb', $user, $pass);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+ini_set('display_errors', 1);
+require_once '../bootstrap.php';
+//$bootstrap = new Boot();
+
+//echo "Waiting...";
