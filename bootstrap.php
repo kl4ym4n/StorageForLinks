e0cabae608322fb88eb77bc39fd4ef16239891f2
Klayman <kl4ym4n@gmail.com>
2016-03-10 18:21:01 +0600

    add class methods

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,15 @@ require_once 'core/generalController.php';
 require_once 'core/route.php';
 require_once 'autoloader.php';
 Route::start();
+
 class Boot
 {
-
+    protected  $user, $password, $connection;
+    public function __construct()
+    {
+        $this->user = 'root';
+        $this->password = 'azarta';
+        $connection = new PDO('mysql:host=localhost; dbname=mydb', $this->user, $this->password);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
 }
