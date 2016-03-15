896c50e79c7d52c3e3f884ca303e3221e5b84fa8
Klayman <kl4ym4n@gmail.com>
2016-03-15 15:32:54 +0600

    add fixes

--- public/index.php
+++ public/index.php
@@ -1,18 +1,18 @@
 <?php
-$user = 'root';
-$pass = 'azarta';
-try
-{
-    $connection = new PDO('mysql:host=localhost; dbname=mydb', $user, $pass);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-
+//$user = 'root';
+//$pass = 'azarta';
+//try
+//{
+//    $connection = new PDO('mysql:host=localhost; dbname=mydb', $user, $pass);
+//    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//}
+//catch (PDOException $e) {
+//    print "Error!: " . $e->getMessage() . "<br/>";
+//    die();
+//}
+//exit;
 ini_set('display_errors', 1);
 require_once '../bootstrap.php';
-//$bootstrap = new Boot();
+$bootstrap = new Boot();
 
 //echo "Waiting...";
