385a5d82f48827baed3f051f8cfa664f077c10b9
Klayman <kl4ym4n@gmail.com>
2016-03-15 15:31:51 +0600

    include scripts

--- views/viewHead.php
+++ views/viewHead.php
@@ -7,6 +7,10 @@ class ViewHead extends View
     {
         $this->template = '<meta charset="utf-8">
         <title>Link storage</title>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script type="text/javascript" src="/js/logout.js"></script>
+
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>';
     }
