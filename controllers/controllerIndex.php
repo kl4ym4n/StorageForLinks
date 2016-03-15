41bc6b78018cec9fbc37c29746405aeb7dca55c1
Klayman <kl4ym4n@gmail.com>
2016-03-15 15:31:19 +0600

    delete spaces

--- controllers/controllerIndex.php
+++ controllers/controllerIndex.php
@@ -3,7 +3,6 @@ class ControllerIndex extends Controller
 {
     public function actionIndex()
     {
-        //$this->view->generate('viewIndex.php', 'viewTemplate.php');
         $this->view = new ViewIndex("Main");
         $this->view->render();
     }
