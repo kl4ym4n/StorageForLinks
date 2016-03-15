93b295c21c8543baec9c87aba594b4896f96f091
Klayman <kl4ym4n@gmail.com>
2016-03-15 15:30:06 +0600

    add boot method

--- core/generalModel.php
+++ core/generalModel.php
@@ -2,10 +2,13 @@
 
 class GeneralModel
 {
-
+    protected  $connection;
+    public function  __construct()
+    {
+        $this->connection = Boot::getConnection();
+    }
     public function printSmth()
     {
         echo "Ololo!";
     }
-
 }
