6453cc3e9362b45db7ce142ca21c3543de24951f
Klayman <kl4ym4n@gmail.com>
2016-03-16 18:12:48 +0600

    add logout js

---
+++ public/js/logout.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $("#logout").click(function()
+    {
+        //alert( "Logout" );
+        $.ajax({
+            type: 'POST',
+            url: 'http://test1/User/Logout',
+            success: function(data) {
+                alert("Success logout");
+                window.location.href = 'http://test1/User/LoginPage'
+            },
+            error: function(xhr, desc, err) {
+                console.log(xhr);
+                console.log("Details: " + desc + "\nError:" + err);
+            }
+        }); // end ajax call
+    });
+});
+
+
+//function ololo()
+//{
+//    alert ("sdfasf");
+//}
+
+
+//function logoutUser()
+//{
+//    alert("adsfsadfsg");
+//
+//    //var button = document.getElementById("logout");
+//    //alert("olol");
+//    //
+//    //var func = function () {
+//    //    alert("Logout!!");
+//    //}
+//    //button.addEventListener("click", func, false);
+//
+//
+//}
