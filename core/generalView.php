95dfd08a113e1ec163a242b9a045090e1089adb7
Klayman <kl4ym4n@gmail.com>
2016-03-23 16:55:04 +0600

    change path

--- core/generalView.php
+++ core/generalView.php
@@ -24,7 +24,7 @@ class View
 
     function generate($content_view, $template_view, $data = null)
     {
-        include 'views/'.$template_view;
+        include '../views/'.$template_view;
     }
 
     function echoActiveClass($requestUri)
