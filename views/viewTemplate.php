f3fd3746304cee4410362c4711af44733716fcce
Klayman <kl4ym4n@gmail.com>
2016-03-23 16:56:17 +0600

    change path

--- views/viewTemplate.php
+++ views/viewTemplate.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="utf-8">
     <title>Link storage</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
 
-<?php include 'views/'.$content_view; ?>
+<?php include '../views/'.$content_view; ?>
 </body>
 </html>
