9775e1b5c77bbe5f3af06cc1d9b4a95ad33dab97
Klayman <kl4ym4n@gmail.com>
2016-03-25 18:03:09 +0600

    add methods

--- core/generalController.php
+++ core/generalController.php
@@ -1,16 +1,34 @@
 <?php
 class Controller
 {
-
     public $model;
     public $view;
 
     function __construct()
     {
         $this->view = new View();
+        $this->model = new GeneralModel();
     }
 
     function actionIndex()
     {
     }
+
+    function allowedAction($actionName, $roleID)
+    {
+        $query = $this->model->getConnection()->prepare("SELECT Permissions.actions FROM Permissions WHERE role_id ='$roleID'");
+        $query->execute();
+        $rowCount = $query->rowCount();
+        $access = false;
+        foreach ($query as $result)
+        {
+            if ($actionName == $result['actions'])
+            {
+                $access = true;
+            }
+        }
+        return $access;
+    }
+        //return $permissions;
+
 }
